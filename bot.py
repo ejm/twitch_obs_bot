bd7e28e68ef2d520c68553cbfe1bfb1d5180c64b
Evan J. Markowitz <evan.markowitz99@gmail.com>
2020-10-24 18:03:08 -0400

    Update license info to account for username change

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 '''
-Copyright 2018 Evan Markowitz (techkid6)
+Copyright 2018-2020 Evan Markowitz (ejm)
 Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
